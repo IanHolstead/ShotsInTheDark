6a668090ecca156a042c1c6f7fe435b0402b908b
Ian Holstead <ian.holstead@gmail.com>
2017-03-14 23:28:07 -0400

    Removed old code and got new stuff working (mostly. I think)

--- Assets/Scripts/PlayerMangement/PlayerProfile.cs
+++ Assets/Scripts/PlayerMangement/PlayerProfile.cs
@@ -2,54 +2,62 @@
 using System.Collections.Generic;
 //using XInputDotNetPure;
 using ControlWrapping;
-public enum Controls
+public enum ActionControl
 {
+    menuSelect,
+    menuBack,
+    menuStart,
     shoot,
     light,
     pause,
-    select,
-    back
+}
+
+public enum AxisControl
+{
+    move,
+    aim
 }
 
 //TODO: OK look, this is broken, deal with it later
 
 public class PlayerProfile {
     string name;
-    Dictionary<Controls, List<Button>> inputMapping;
+    Dictionary<ActionControl, List<ActionKeyCode>> actionMapping;
+    Dictionary<AxisControl, List<AxisCode>> axisMapping;
 
     //public delegate
 
-    public PlayerProfile()
+    public void NewPlayerProfile()
     {
-        foreach (Controls control in System.Enum.GetValues(typeof(Controls)))
+        actionMapping = new Dictionary<ActionControl, List<ActionKeyCode>>();
+        axisMapping = new Dictionary<AxisControl, List<AxisCode>>();
+        foreach (ActionControl control in System.Enum.GetValues(typeof(ActionControl)))
         {
-            inputMapping[control] = new List<Button>();
+            actionMapping[control] = new List<ActionKeyCode>();
+        }
+
+        foreach (AxisControl control in System.Enum.GetValues(typeof(AxisControl)))
+        {
+            axisMapping[control] = new List<AxisCode>();
         }
     }
 
-    public bool setControl(Controls control)
+    public bool setActionControl(ActionControl control)
     {
         return false;
     }
 
-    private static Dictionary<Controls, List<Button>> getDefaultMapping()
+    private void SetDefaultMapping()
     {
-        Dictionary<Controls, List<Button>> mapping = new Dictionary<Controls, List<Button>>();
-        
-        foreach (Controls control in System.Enum.GetValues(typeof(Controls)))
-        {
-            mapping[control] = new List<Button>();
-        }
-
-        XInputDotNetPure.GamePadState bla = XInputDotNetPure.GamePad.GetState(XInputDotNetPure.PlayerIndex.One);
-
-        //mapping[Controls.shoot].Add(Button.);
-        //mapping[Controls.light].Add("test");
-        //mapping[Controls.pause].Add("test");
-        //mapping[Controls.select].Add("test");
-        //mapping[Controls.back].Add("test");
-
-        return mapping;
+        NewPlayerProfile();
+        actionMapping[ActionControl.menuSelect].Add(ActionKeyCode.GamepadA);
+        actionMapping[ActionControl.menuBack].Add(ActionKeyCode.GamepadB);
+        actionMapping[ActionControl.menuStart].Add(ActionKeyCode.GamepadStart);
+        actionMapping[ActionControl.shoot].Add(ActionKeyCode.GamepadA);
+        actionMapping[ActionControl.shoot].Add(ActionKeyCode.GamepadLeftTrigger);
+        actionMapping[ActionControl.shoot].Add(ActionKeyCode.GamepadRightTrigger);
+        actionMapping[ActionControl.light].Add(ActionKeyCode.GamepadB);
+        actionMapping[ActionControl.pause].Add(ActionKeyCode.GamepadStart);
     }
     
 
